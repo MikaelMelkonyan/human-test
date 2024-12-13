a470cf04f9ea25b9fba9e35a80a873630ffcd14e
Mikael <melkonyan2311@icloud.com>
2024-12-12 19:53:55 -0600

    WIP

---
+++ Human-Test/Human-Test/Human_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Human_TestApp.swift
+//  Human-Test
+//
+//  Created by Mikael Melkonyan on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Human_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
